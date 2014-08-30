e1abcd2afe511b5e5d26589bd9d7d6b5671e412b
Wei <weiguxp@gmail.com>
2014-08-30 10:09:41 +0800

    Changed left and right motion

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,18 +4,26 @@ public class Player : MonoBehaviour
 {
 	// The force which is added when the player jumps
 	// This can be changed in the Inspector window
-	public Vector2 jumpForce = new Vector2(0, 300);
-	
+	public Vector2 jumpForcel = new Vector2(0, 300);
+	public Vector2 jumpForcer = new Vector2(0, 300);
 	// Update is called once per frame
 	void Update ()
 	{
 		// Jump
-		if (Input.GetKeyUp("space"))
+		if (Input.GetKeyUp("left"))
 		{
 			rigidbody2D.velocity = Vector2.zero;
-			rigidbody2D.AddForce(jumpForce);
+			rigidbody2D.AddForce(jumpForcel);
 		}
-		
+
+	
+		// Jump
+		if (Input.GetKeyUp("right"))
+		{
+			rigidbody2D.velocity = Vector2.zero;
+			rigidbody2D.AddForce(jumpForcer);
+		}
+
 		// Die by being off screen
 		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
 		if (screenPosition.y > Screen.height || screenPosition.y < 0)
