fff41e91a461ea72a485cf564d2e93a0321486a7
wei <weiguxp@gmail.com>
2014-09-03 14:03:50 +0800

    Experimenting with game state

--- Assets/Scripts/CopterController.cs
+++ Assets/Scripts/CopterController.cs
@@ -2,6 +2,15 @@
 
 public class CopterController : MonoBehaviour
 {
+	public  enum GameState
+	{
+		initiate,
+		play,
+		playerdeath,
+	}
+	
+	public GameState currentState;
+	public static CopterController CS;
 	// The force which is added when the player jumps
 	// This can be changed in the Inspector window
 	public Vector2 jumpForcel = new Vector2(0, 300);
@@ -13,13 +22,54 @@ public class CopterController : MonoBehaviour
 	private float playerMinVelocity = 1.5f;
 
 
+	// Direction enumerator
+
+
+	void Start()
+	{
+		CS = this;
+		DontDestroyOnLoad(this);
+		currentState = GameState.play;
+	}
+
+
 	// Update is called once per frame
 	void Update ()
 	{
+		if (currentState == GameState.play){
 
-		//Accelerator 
+		CopterMomentum (directionRight);
 
-		if (directionRight == true)
+		//Spacebar controls direction
+		if (Input.GetMouseButtonDown (0)) {
+				if (directionRight == true) {
+						directionRight = false;
+						if (leftVelocity > -playerMinVelocity) {
+								leftVelocity = -2f;
+						}
+				} else {
+						directionRight = true;
+						if (rightVelocity < playerMinVelocity) {
+								rightVelocity = 2f;
+						}
+				}
+
+		}
+
+		// Die by being off screen
+		Vector2 screenPosition = Camera.main.WorldToScreenPoint (transform.position);
+		if (screenPosition.y > Screen.height || screenPosition.y < 0) {
+						Die ();
+				}
+			}
+	}
+
+	void CopterMomentum (bool copterDirection)
+	{
+		//This part of the script gives the impression of fake momentum of the object
+		//We break down the velocity of the object into two separate components and keep them in floats.
+
+		if (copterDirection == true)
 		{
 			rightVelocity = rightVelocity + playerAcceleration;
 			leftVelocity = (float)leftVelocity * 0.9f;
@@ -33,31 +83,7 @@ public class CopterController : MonoBehaviour
 			rigidbody2D.velocity = new Vector2(0, leftVelocity);
 			Debug.Log (rightVelocity);
 		}
-
-
-		//Spacebar controls direction
-		if (Input.GetMouseButtonDown(0))
-		{
-			if (directionRight == true)
-			{
-				directionRight = false;
-				if (leftVelocity > -playerMinVelocity){ leftVelocity = -2f ;}
-			}
-			else
-			{
-				directionRight = true;
-				if (rightVelocity < playerMinVelocity) { rightVelocity = 2f ;}
-			}
-
 		}
-
-		// Die by being off screen
-		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
-		if (screenPosition.y > Screen.height || screenPosition.y < 0)
-		{
-			Die();
-		}
-	}
 	
 	// Die by collision
 	void OnCollisionEnter2D(Collision2D other)
@@ -67,6 +93,7 @@ public class CopterController : MonoBehaviour
 	
 	void Die()
 	{
-		Application.LoadLevel(Application.loadedLevel);
+		currentState = GameState.playerdeath;
 	}
 }
+
